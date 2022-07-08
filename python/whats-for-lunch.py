9844e5c21bbca0914786af7c780f22fd9445b5c3
timcbarnes <timbarnes@protonmail.com>
2022-07-08 07:57:35 -0500

    Initial commit

---
+++ python/whats-for-lunch.py
@@ -0,0 +1,14 @@
+# Tim Barnes
+# v0.0.1
+# 2022-07-08
+#
+# Find restaurants within a given geographic area and choose one at random.
+# The user enters their address and desired search radius.
+# The script uses the yelp api to find restaurants within the specified radius
+# around the user's address.
+
+def main():
+    return
+
+
+main()
